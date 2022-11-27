3bd86e99f9e9887d585bf1c5f145bb37d30a16e3
EpicBatCow <randallkpowers@gmail.com>
2022-11-26 22:59:35 -0700

    ignore index fix

--- main.py
+++ main.py
@@ -232,37 +232,37 @@ for index, row in df.iterrows():
     Id += 1
     if row['Country (mentioned)'] in countriesZero:
         print("country", 0)
-        submission.append({'Id': Id, "Category": 0})
+        submission.append({'Id': Id, "Category": 0},ignore_index = True)
     elif row['Country (mentioned)'] in countriesOne:
         print("country", 1)
-        submission.append({'Id': Id, "Category": 1})
+        submission.append({'Id': Id, "Category": 1},ignore_index = True)
     elif row['Country (mentioned)'] in countriesTwo:
         print("country", 2)
-        submission.append({'Id': Id, "Category": 2})
+        submission.append({'Id': Id, "Category": 2},ignore_index = True)
     elif row['Country (mentioned)'] in countriesThree:
         print("country", 3)
-        submission.append({'Id': Id, "Category": 3})
+        submission.append({'Id': Id, "Category": 3},ignore_index = True)
     elif row['Source'] in sourceZero:
         print("Source", 0)
-        submission.append({'Id': Id, "Category": 0})
+        submission.append({'Id': Id, "Category": 0},ignore_index = True)
     elif row['Source'] in sourceOne:
         print("Source", 1)
-        submission.append({'Id': Id, "Category": 1})
+        submission.append({'Id': Id, "Category": 1},ignore_index = True)
     elif row['Source'] in sourceTwo:
         print("Source", 2)
-        submission.append({'Id': Id, "Category": 2})
+        submission.append({'Id': Id, "Category": 2},ignore_index = True)
     elif row['Source'] in sourceThree:
         print("Source", 3)
-        submission.append({'Id': Id, "Category": 3})
+        submission.append({'Id': Id, "Category": 3},ignore_index = True)
     elif checkURL(urlZero, row['Fact-checked Article']) == 1:
         print("URL", 0)
-        submission.append({'Id': Id, "Category": 0})
+        submission.append({'Id': Id, "Category": 0},ignore_index = True)
     elif checkURL(urlOne, row['Fact-checked Article']) == 1:
         print("URL", 1)
-        submission.append({'Id': Id, "Category": 1})
+        submission.append({'Id': Id, "Category": 1},ignore_index = True)
     elif checkURL(urlTwo, row['Fact-checked Article']) == 1:
         print("URL", 2)
-        submission.append({'Id': Id, "Category": 2})
+        submission.append({'Id': Id, "Category": 2},ignore_index = True)
     else:
     #selenium open/save text of article
         checkedURL = row['Fact-checked Article']
@@ -286,11 +286,11 @@ for index, row in df.iterrows():
             elif("unproven" in TranslatedText):
                 score = 3
                 numThree += 1
-            submission.append({'Id': Id, "Category": score})
+            submission.append({'Id': Id, "Category": score},ignore_index = True)
         except WebDriverException:
             print() # website is down
             score = 0
-            submission.append({'Id': Id, "Category": score})
+            submission.append({'Id': Id, "Category": score},ignore_index = True)
 
 print("Number of 0's from scraping")
 print(numZeros)
